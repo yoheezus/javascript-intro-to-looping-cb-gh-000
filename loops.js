3b8170ec25cf5cb1482c2db3cd3addec657bfdda
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 22:40:45 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,11 @@
 var theArray = []
 
-for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-        theArray.push("I am 1 strange loop.")
-    } else {
-        theArray.push(`I am ${i} strange loops.`)
+function forLoop(array) {
+    for (let i = 0; i < 25; i++) {
+        if (i === 1) {
+            theArray.push("I am 1 strange loop.")
+        } else {
+            theArray.push(`I am ${i} strange loops.`)
+        }
     }
-
 }
