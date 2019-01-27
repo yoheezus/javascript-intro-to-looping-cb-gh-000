552cc94b78faf0b9a23130bf62668fed9a024d23
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-27 22:44:05 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ var theArray = []
 
 function forLoop(array) {
     for (var i = 0; i < 25; i++) {
-        if (i === 0) {
+        if (i === 1) {
             array.push("I am 1 strange loop.")
         } else {
             array.push(`I am ${i} strange loops.`)
